c7f088f9ec7e7f46738ea5800a719b632f723f4e
Lyudmila <kudaevalyudmila@gmail.com>
2022-12-22 15:39:20 +0300

    metod PrintArray

---
+++ Program/Program.cs
@@ -0,0 +1,10 @@
+﻿void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
