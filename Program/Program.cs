2994d81834a29db68db578f1640cab1f031630a1
Lyudmila <kudaevalyudmila@gmail.com>
2022-12-22 15:48:17 +0300

    Program

--- Program/Program.cs
+++ Program/Program.cs
@@ -28,3 +28,15 @@ string[] SortingArray(string[] arr1, int n)
     }
     return arr2;
 }
+Console.WriteLine("Введите размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrString1 = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1} элемент массива:");
+    arrString1[i] = Console.ReadLine();
+}
+PrintArray(arrString1);
+Console.Write("->");
+string[] arrString2 = SortingArray(arrString1, 3);
+PrintArray(arrString2);
